da4f0cd85c3aff29d60390d36b80c2e55e13b45f
rafael7238 <rafael7238@hotmail.com>
2021-11-06 16:57:04 -0300

    Add Docker

--- Flask_SQLALCEMY/app.py
+++ Flask_SQLALCEMY/app.py
@@ -1,27 +1,117 @@
-#pip install flask
-#pip install flask_sqlalchemy
-#pip install mysql-connector-python
-#pip install mysqlclient
+# pip install flask
+# pip install flask_sqlalchemy
+# pip install mysql-connector-python
+# pip install mysqlclient
 
-from flask import Flask,Response,Request
+from operator import truediv
+from os import stat
+import re
+from flask import Flask, Response, request
 from flask_sqlalchemy import SQLAlchemy
 import mysql.connector
 import json
 
-app=Flask(__name__)
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"]=True
-                                      #mysql://username:password@server/db
-app.config["SQLALCHEMY_DATABASE_URI"]="mysql://root:rafael19@localhost/teste"
+app = Flask(__name__)
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True
+# mysql://username:password@server/db
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql://root:rafael19@localhost/curso_python"
 
 db = SQLAlchemy(app)
 
+
 class Usuario(db.Model):
-    id = db.Column(db.Integer,primary_key=True)
+    id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(50))
     email = db.Column(db.String(100))
 
-#db.create_all()
+    def to_json(self):
+        return{"id": self.id, "nome": self.nome, "email": self.email}
 
-#Selecionar Tudo
-#Selecionar Um
-#Selec
+# db.create_all()
+
+# Selecionar Tudo
+
+
+@app.route("/usuarios", methods=["GET"])
+def seleciona_usuarios():
+    usuarios_objetos = Usuario.query.all()
+    usuarios_json = [usuario.to_json() for usuario in usuarios_objetos]
+    print(usuarios_objetos, type(usuarios_objetos))
+    return gera_response(200, "usuarios", usuarios_json, "ok")
+
+@app.route('/',methods=["GET"])
+def seleciona_usuarios2():
+    usuarios_objetos = Usuario.query.all()
+    usuarios_json = [usuario.to_json() for usuario in usuarios_objetos]
+    print(usuarios_objetos, type(usuarios_objetos))
+    return gera_response(200, "usuarios", usuarios_json, "ok")
+
+# Selecionar Um
+
+
+@app.route("/usuario/<id>", methods=["GET"])
+def seleciona_usuario(id):
+    usuario_objeto = Usuario.query.filter_by(id=id).first()
+    usuarios_json = usuario_objeto.to_json()
+    return gera_response(200, "usuario", usuarios_json, "ok")
+
+@app.route("/teste/", methods=["GET"])
+def seleciona_usuario2():
+    return gera_response(200, "usuario", {"nome":"nada"}, "ok")
+
+
+# Cadastrar
+@app.route("/usuario", methods=["POST"])
+def cria_usuario():
+    body = request.get_json()
+    try:
+        usuario = Usuario(nome=body["nome"], email=body["email"])
+        db.session.add(usuario)
+        db.session.commit()
+        return gera_response(201, "usuario", usuario.to_json(), "Criado com Sucesso")
+    except Exception as e:
+        print(e)
+        return gera_response(400, "usuario", {}, "Erro ao cadastrar")
+
+
+@app.route("/usuario/<id>", methods=["PUT"])
+def atualiza_usuario(id):
+    usuario_objeto = Usuario.query.filter_by(id=id).first()
+    body = request.get_json()
+
+    try:
+        if("nome" in body):
+            usuario_objeto.nome = body["nome"]
+        if("email" in body):
+            usuario_objeto.email = body["email"]
+        db.session.add(usuario_objeto)
+        db.session.commit()
+        return gera_response(200, "usuario", usuario_objeto.to_json(), "Atualizado com Sucesso")
+    except Exception as e:
+        print(e)
+        return gera_response(400, "usuario", {}, "Erro ao atualizar")
+
+
+@app.route("/usuario/<id>", methods=['DELETE'])
+def deleta_usuario(id):
+    usuario_objeto = Usuario.query.filter_by(id=id).first()
+    try:
+        db.session.delete(usuario_objeto)
+        db.session.commit()
+        return gera_response(200, "usuario", usuario_objeto.to_json(), "Deletado com sucesso")
+    except Exception as e:
+        print("Erro", e)
+        return gera_response(400, "usuario", {}, "Erro ao Deletar")
+
+
+def gera_response(status, nome_do_conteudo, conteudo, mensagem=False):
+    body = {}
+    body[nome_do_conteudo] = conteudo
+    if(mensagem):
+        body["mensagem"] = mensagem
+    return Response(json.dumps(body), status=status, mimetype="application/json")
+
+app.run()
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ Ptyhon_para_devops/1_Estrutura_de_Decisao/decisa_if.py
@@ -0,0 +1,10 @@
+idade = 15
+boleano = False
+if idade<15:
+    print("Menor que 15")
+else:
+    print("Maior ou igual a 15")
+
+if boleano is True:
+    print("Valor True")
+
